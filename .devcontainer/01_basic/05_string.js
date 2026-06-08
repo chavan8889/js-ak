56bd300aec0bf9bc35eae47fa7079d1c4eb37b65
akshay chavan <ac8972259@gmail.com>
2026-06-08 10:57:00 +0000

    string

---
+++ .devcontainer/01_basic/05_string.js
@@ -0,0 +1,16 @@
+const name = "akshay"
+const age = 23
+console.log(`My name is ${name} and I am ${age} years old.`)
+
+const str1 = "Hello"
+const str2 = "World"
+const combined = `${str1} ${str2}`
+console.log(combined) // Output: Hello World
+
+const multiLineString = `This is a
+multi-line string.`
+console.log(multiLineString)  
+
+
+
+
