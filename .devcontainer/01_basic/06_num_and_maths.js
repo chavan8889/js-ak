54de3cfa4977c3020500eb61eb4e04ff6a9f5864
akshay chavan <ac8972259@gmail.com>
2026-06-09 07:20:23 +0000

    math and num

---
+++ .devcontainer/01_basic/06_num_and_maths.js
@@ -0,0 +1,31 @@
+console.log(Math.sqrt(25));    // 5
+console.log(Math.pow(2, 3));   // 8
+console.log(Math.max(10, 20)); // 20
+console.log(Math.min(10, 20)); // 10
+console.log(Math.round(4.7)) // 5
+console.log(Math.floor(4.9));  // 4
+console.log(Math.ceil(4.1));   // 5
+console.log(Math.random());    
+
+
+const  otherNumber = 123.8966
+console.log(otherNumber.toPrecision(3))
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString())
+
+
+//++++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-258))// It converts the negative number to positive number
+console.log(Math.ceil(4.2))// It rounds the number up to the nearest integer
+console.log(Math.floor(4.8))// It rounds the number down to the nearest integer
+console.log(Math.round(4.5))// It rounds the number to the nearest integer
+console.log(Math.max(1, 5, 3, 9, 2))// It returns the largest number from a set of numbers
+console.log(Math.min(1, 5, 3, 9, 2))// It returns the smallest number from a set of numbers
+console.log(Math.pow(2, 3))// It returns the base to the exponent power
+console.log(Math.sqrt(16))// It returns the square root of a number
+console.log(Math.random())// It returns a random number between 0 (inclusive) and 1 (exclusive)
+
+
